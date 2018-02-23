40ecaf04120b54da71fc33bc5fde5dd8f6569a57
macak-coding <fzdsaputra@gmail.com>
2018-02-23 17:30:38 +0700

    minor updatee

--- src/id/nullpointr/JmxDataGetter.java
+++ src/id/nullpointr/JmxDataGetter.java
@@ -123,7 +123,6 @@ public class JmxDataGetter implements Runnable{
 			    
 			    if(count % 10 == 0) {
 			    	writer.flush();
-			    	//System.out.println(fullpathFile+" flushing!!");
 			    }
 			    
 			    float currentHeapPresentage = (used / init) * 100;
